666e4d4157157673eb3a5fe917998c7ec3e72a3d
Julian Maierl <scomputerlp@gmail.com>
2015-03-16 19:51:08 +0100

    Renamed the package

---
+++ src/com/jcorn/jcircleparty/Main.java
@@ -0,0 +1,17 @@
+package com.jcorn.jcircleparty;
+
+/**
+ * jCircleParty
+ * 
+ * @author Markus Petritz
+ * @author Julian Maierl
+ * @version 1.0.0
+ * @see http://petritzdesigns.at
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
