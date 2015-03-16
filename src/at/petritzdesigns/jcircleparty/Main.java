9cc7b534c5870d05a5d614ac7cdcbeaa89ce68c1
petritzdesigns <petritzdesigns@gmail.com>
2015-03-16 19:41:42 +0100

    First Commit: Netbeans Project created

---
+++ src/at/petritzdesigns/jcircleparty/Main.java
@@ -0,0 +1,16 @@
+package at.petritzdesigns.jcircleparty;
+
+/**
+ * jCircleParty
+ * 
+ * @author Markus Petritz
+ * @version 1.0.0
+ * @see http://petritzdesigns.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
