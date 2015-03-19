7f69725317e414ab87784b6ae425daff2ccefc8e
SoftwareCreator <jakob.meinhart@gmx.at>
2015-03-19 08:01:19 +0100

    Symbols finished
    HABE FILL POLYGON BENUTZT ^^

--- src/com/jcorn/jcircleparty/client/model/Symbol.java
+++ src/com/jcorn/jcircleparty/client/model/Symbol.java
@@ -17,6 +17,10 @@ public class Symbol {
     private int x, y, size;
     private Color color;
     private final SymbolEnum type;
+    private int feldX[] = new int [10];
+    private int feldY[] = new int [10];
+    private int numberOfPoints = 3;
+    private int border = 30;
 
     public Symbol(SymbolEnum type) {
         this.type = type;
@@ -63,27 +67,41 @@ public class Symbol {
         g2d.setColor(color);
 
         switch (type) {
-            case SQUARE:
-                g2d.fillRect(x, y, size, size);
-                break;
-            case TRIANGLE:
-                break;
-            case HIPSTER_TRIANGLE:
-                break;
-            case OVAL:
-                g2d.fillOval(x + 30, y, size - 100, size);
-                break;
-            case RECTANGLE:
-                g2d.fillRect(x, y + 10, size, size - 20);
-                break;
-            case HEXAGON:
-                break;
-            case DIAMOND:
-                break;
-            case CIRCLE:
-            default:
-                g2d.fillOval(x, y, size, size);
-                break;
+            case SQUARE: g2d.fillRect(x, y, size, size);
+                         break;
+                
+            case TRIANGLE: feldX[0] = border;   feldX[1] = size; feldX[2] = size / 2;
+                           feldY[0] = size; feldY[1] = size; feldY[2] = border;
+                           g2d.fillPolygon(feldX, feldY, numberOfPoints);
+                           break;
+                
+            case HIPSTER_TRIANGLE: feldX[0] = border; feldX[1] = size; feldX[2] = size / 2;
+                                   feldY[0] = border; feldY[1] = border;   feldY[2] = size;
+                                   g2d.fillPolygon(feldX, feldY, numberOfPoints);
+                                   break;
+
+            case OVAL: g2d.fillOval(x + border, y + border, size - 100, size);
+                       break;
+                
+            case RECTANGLE: g2d.fillRect(x + border, y + border, size, size - 100);
+                            break;
+                
+            case HEXAGON: feldX[0] = border;   feldX[1] = size / 4; feldX[2] = (size / 4) * 3; feldX[3] = size - border; feldX[4] = (size / 4) * 3; feldX[5] = size / 4;
+                          feldY[0] = size / 2; feldY[1] = border;   feldY[2] = border;         feldY[3] = size / 2;      feldY[4] = size - border;  feldY[5] = size - border;
+                          numberOfPoints = 6;
+                          g2d.fillPolygon(feldX, feldY, numberOfPoints);
+                          break;
+                
+            case DIAMOND: feldX[0] = size / 2; feldX[1] = (size / 4) * 3; feldX[2] = size / 2;      feldX[3] = border;
+                          feldY[0] = border;   feldY[1] = size / 2;       feldY[2] = size - border; feldY[3] = size / 2;     
+                          numberOfPoints = 4;
+                          g2d.fillPolygon(feldX, feldY, numberOfPoints);
+                          break;
+                
+            case CIRCLE: g2d.fillOval(x + border, y + border, size - border, size - border);
+                         break;
+            default: g2d.fillOval(x + border, y + border, size - border, size - border);
+                     break;
         }
     }
 }
